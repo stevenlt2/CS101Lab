584f13b704efd1f4841b8a2dc486932ab890510f
stevenlt2 <113187124+stevenlt2@users.noreply.github.com>
2022-09-16 10:01:03 -0500

    Add files via upload

--- Lab3.py
+++ Lab3.py
@@ -56,6 +56,33 @@ while game:
     answer = input('Do you want to play again? Y to continue, N to quit ==>')
     if answer == "Y" or answer == 'y':
         print('\nPlease think of a number between and including 1 and 100.')
+        print()
+#remainder for 3
+        remainder_three = int(input('What is the remainder when your number is divided by 3 ?'))
+        while(remainder_three < 0 or remainder_three >= 3):
+            if remainder_three < 0:
+                print('The value entered must be 0 or greater')
+            elif remainder_three >= 3:
+                print('The value entered must be less than 3')
+            remainder_three = int(input('What is the remainder when your number is divided by 3 ?'))
+#remainder for 5
+        remainder_five = int(input('What is the remainder when your number is divided by 5 ?'))
+        while(remainder_five < 0 or remainder_five >= 5):
+            if remainder_five < 0:
+                print('The value entered must be 0 or greater')
+            elif remainder_five >= 5:
+                print('The value entered must be less than 5')
+            remainder_five = int(input('What is the remainder when your number is divided by 5 ?'))
+#remainder for 7
+        remainder_seven = int(input('What is the remainder when your number is divided by 7 ?'))
+        while(remainder_seven < 0 or remainder_seven >= 7):
+            if remainder_seven < 0:
+
+                print('The value entered must be 0 or greater')
+            elif remainder_seven >= 7:
+                print('The value entered must be less than 7')
+
+            remainder_seven = int(input('What is the remainder when your number is divided by 7 ?'))
         
     elif answer == "N" or answer == 'n':
         game = False
